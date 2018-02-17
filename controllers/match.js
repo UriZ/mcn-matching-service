93e64e9f2224e0dd0a745197bc855ba52bdedef9
URIZ <uriz@il.ibm.com>
2018-02-17 13:37:03 +0200

    basic match impl - get all users from db, filter on fb friends

--- controllers/match.js
+++ controllers/match.js
@@ -23,8 +23,8 @@ let getFBFriends = (userId, fbAccessToken)=>{
 
         requestPromise(options)
             .then(function (result) {
-                console.log("user's friends list recieved");
-
+                console.log("user's friends list received");
+                console.log(result);
                  resolve(result);
             })
             .catch(function (err) {
@@ -33,12 +33,40 @@ let getFBFriends = (userId, fbAccessToken)=>{
                 reject(err);
             });
     });
+}
 
 
+/**
+ * get the basic match from db - all records that match the preferences
+ * @returns {Promise<any>}
+ */
+let basicMatch = ()=>{
+
+    return new Promise((resolve, reject)=>{
+
+        // options for user service call
+        let options = {
+            method: 'get',
+            uri: process.env.USER_SERVICE_URL,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
 
 
+        requestPromise(options)
+            .then(function (result) {
+                console.log("success getting users from db");
+                console.log(result);
+                resolve(result);
 
-
+            })
+            .catch(function (err) {
+                console.log("error getting users  from db " +  err);
+                reject(err);
+            });
+    });
 
 }
 
@@ -49,14 +77,37 @@ let getFBFriends = (userId, fbAccessToken)=>{
  */
 module.exports.findMatchForUser = function findMatchForUser (req, res) {
 
+    // get fb id and token from request
+    const id = req.swagger.params.fb_user_id.value;
+    const token  = req.swagger.params.fbToken.value;
 
-    getFBFriends(req.swagger.params.fb_user_id.value, req.swagger.params.fbToken.value).then((result)=>{
+    // get fb friends and basic match from db
+    Promise.all([basicMatch(),getFBFriends(id, token)]).then((results)=>{
 
-        res.status(200).send(result);
+        console.log("Array1 " + results[0]);
+        console.log("Array2 " + results[1]);
+        console.log("array 2 data :" + results[1].data);
 
-    }).catch((error)=>{
-        res.status(500).send(error);
+        // create an array of friends ids
+        let arrOfFriendsIds = results[1].data.map((elem)=>{
+            return(elem.id);
+        });
+        console.log("array of ids " + arrOfFriendsIds);
+
+        // fitler the results of the basic match to contain only elements in the friends array
+        let filteredArr = results[0].filter((element, index, array)=>{
+
+            console.log("checking for " + element._id);
+            return arrOfFriendsIds.includes(element._id);
+
+        });
+        console.log(filteredArr);
+        res.status(200).send(filteredArr);
+    }).catch((err)=>{
+        res.status(500).send(err);
     });
+
+
 };
 
 
